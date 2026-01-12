230eb8b06b80bea4df717b640fff6bfbf90ea436
Shema Alphonse <ashema330@gmail.com>
2026-01-12 15:20:34 +0200

    Created Prescription class with fields: prescriptionId, patient, prescriptionDate, notes

--- src/main/java/hospital/hospital_management_system/model/Prescriptions.java
+++ src/main/java/hospital/hospital_management_system/model/Prescriptions.java
@@ -1,4 +1,12 @@
 package hospital.hospital_management_system.model;
 
+import java.time.LocalDateTime;
+
 public class Prescriptions {
+    private Long prescriptionId;
+    private Patient patient;
+    private LocalDateTime prescriptionDate;
+    private String notes;
+
+
 }
