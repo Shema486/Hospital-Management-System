813fade20a1201eee6bedfc88fb3adccd8a21ef3
Shema Alphonse <ashema330@gmail.com>
2026-01-12 12:01:31 +0200

    initial commit

---
+++ src/main/java/hospital/hospital_management_system/launcher/Launcher.java
@@ -0,0 +1,10 @@
+package hospital.hospital_management_system.launcher;
+
+import hospital.hospital_management_system.HelloApplication;
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
