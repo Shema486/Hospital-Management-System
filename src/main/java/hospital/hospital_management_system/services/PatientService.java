c6c4e60fb6656b6a383389411d63855f748bc0ca
Shema Alphonse <ashema330@gmail.com>
2026-01-13 12:57:38 +0200

    refactor(service): searching patient by their lastName using caching

--- src/main/java/hospital/hospital_management_system/services/PatientService.java
+++ src/main/java/hospital/hospital_management_system/services/PatientService.java
@@ -1,6 +1,25 @@
 package hospital.hospital_management_system.services;
 
-public class PatientService {
+import hospital.hospital_management_system.dao.PatientDAO;
+import hospital.hospital_management_system.model.Patient;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PatientService {
+    private PatientDAO patientDAO = new PatientDAO();
+    private Map<Long, Patient> patientCaching = new HashMap<>();
+
+    public List<Patient> searchingPatientByLastName(String lastName){
+
+        List<Patient> allPatients = patientDAO.searchPatientByLastName(lastName);
+
+        for (Patient p:allPatients){
+            patientCaching.put(p.getPatientId(),p );
+        }
+        return allPatients;
+
+    }
 
 }
