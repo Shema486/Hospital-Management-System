287d700ab33165f34cbacb093c360d8d07a3843d
Shema Alphonse <ashema330@gmail.com>
2026-01-14 06:42:08 +0200

    refactor: renamed from helloApplication and load PatientView

---
+++ src/main/java/hospital/hospital_management_system/MainApp.java
@@ -0,0 +1,26 @@
+package hospital.hospital_management_system;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(
+                getClass().getResource("PatientView.fxml")
+        );
+
+        Scene scene = new Scene(root, 1000, 600);
+        stage.setTitle("Hospital Management System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
