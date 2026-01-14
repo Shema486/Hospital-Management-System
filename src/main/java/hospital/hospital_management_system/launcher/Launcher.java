4055211a55972b76f3d63afcc951de7cca56d095
Shema Alphonse <ashema330@gmail.com>
2026-01-14 06:56:49 +0200

    refactor(launcher): renamed launcher class from HelloApplication to MainApp

--- src/main/java/hospital/hospital_management_system/launcher/Launcher.java
+++ src/main/java/hospital/hospital_management_system/launcher/Launcher.java
@@ -1,10 +1,10 @@
 package hospital.hospital_management_system.launcher;
 
-import hospital.hospital_management_system.HelloApplication;
+import hospital.hospital_management_system.MainApp;
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        Application.launch(MainApp.class, args);
     }
 }
