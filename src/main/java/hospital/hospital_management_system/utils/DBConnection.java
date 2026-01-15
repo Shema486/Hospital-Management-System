cb095feeecf581a23e791fb904b620309726dca2
Shema Alphonse <ashema330@gmail.com>
2026-01-15 12:36:30 +0200

    refactor(utils): strong security for data database

--- src/main/java/hospital/hospital_management_system/utils/DBConnection.java
+++ src/main/java/hospital/hospital_management_system/utils/DBConnection.java
@@ -6,17 +6,16 @@ import java.sql.SQLException;
 
 public final class DBConnection {
 
-    private static final String DATABASE_NAME = "hospital_db";
-    private static final String DATABASE_USER = "postgres";
-    private static final String DATABASE_PASSWORD = "Alphonse";
+    private static final String DB_HOST = EnvLoader.get("DB_HOST", "localhost");
+    private static final String DB_PORT = EnvLoader.get("DB_PORT", "5432");
+    private static final String DB_NAME = EnvLoader.get("DB_NAME", "hospital_db");
+    private static final String DB_USER = EnvLoader.get("DB_USER", "postgres");
+    private static final String DB_PASSWORD = EnvLoader.get("DB_PASSWORD", "Alphonse");
 
     private DBConnection() {} // Prevent instantiation
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/" + DATABASE_NAME,
-                DATABASE_USER,
-                DATABASE_PASSWORD
-        );
+        String url = String.format("jdbc:postgresql://%s:%s/%s", DB_HOST, DB_PORT, DB_NAME);
+        return DriverManager.getConnection(url, DB_USER, DB_PASSWORD);
     }
 }
